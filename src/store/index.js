fb726061e10ddd3d0ec4912ffff8daa6f82c3c9d
Shokhruh <shokhruh1197@gmail.com>
2024-05-04 16:51:41 +0500

    state

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state () {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    },
+    decrement (state) {
+        state.count--
+    },
+  }
+})
+
+export default store
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
-        Home Page
+        <h1>Count: {{ $store.state.count }}</h1>
+        <button class="btn btn-success" @click="increaseHandler">Add</button>
+        <button class="btn btn-danger" @click="decreaseHandler">Remove</button>
     </div>
 </template>
 <script>
 export default {
-    
+    methods: {
+        increaseHandler() {
+            this.$store.commit('increment')
+        },
+        decreaseHandler() {
+            this.$store.commit('decrement')
+        }
+    }
 }
 </script>
 <style>
